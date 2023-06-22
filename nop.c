044f3eb0381020ca6ceb9cdfa3dd42f98fa4b6be
danieljonesobot <daniel.jonesobot@gmail.com>
2023-06-22 06:28:43 -0700

    created nop.c file

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+  *nope- does not do anything
+  *@hd: head of stack
+  *@cter: line number
+ */
+void nope(stack_t **hd, unsigned int cter)
+{
+	(void) cter;
+	(void) hd;
+}
